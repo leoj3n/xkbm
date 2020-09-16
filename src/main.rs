834d13fb97b0031903ad16be20da065d6ecbfbb4
Joel Kuzmarski <leoj3n@gmail.com>
2020-09-16 11:35:39 -0500

    cargo clippy/fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-#![allow(unused)]
+use gilrs::{Axis, Button, Event, Gamepad, Gilrs};
 use std::collections::HashMap;
-use gilrs::{Gilrs, Gamepad, Button, Event, Axis};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
@@ -44,7 +43,11 @@ enum Letter {
     X,
     Y,
     Z,
+    Space,
 }
+
+const MIN: f32 = 0.9;
+const MAX: f32 = 0.5;
 
 #[derive(Debug)]
 struct Combo {
@@ -53,198 +56,301 @@ struct Combo {
 }
 
 impl Combo {
-    fn check_bounds_and_push(&mut self, gamepad: Gamepad, x: Axis, y: Axis, north: Stick, east: Stick, south: Stick, west: Stick) {
-        let min = 0.9;
+    #[allow(clippy::too_many_arguments)]
+    fn check_bounds_and_push(
+        &mut self,
+        x: f32,
+        y: f32,
+        north: Stick,
+        east: Stick,
+        south: Stick,
+        west: Stick,
+    ) {
         let mut stick = None;
 
-        match (gamepad.value(x), gamepad.value(y)) {
-          (x, y) => {
-              if x.abs() > y.abs() {
-                  if x >= min {
-                      stick = Some(east);
-                  } else if x <= -min {
-                      stick = Some(west);
-                  }
-              } else {
-                  if y >= min {
-                      stick = Some(north);
-                  } else if y <= -min {
-                      stick = Some(south);
-                  }
-              }
-          },
-          _ => {}
+        if x.abs() > y.abs() {
+            if x >= MIN {
+                stick = Some(east);
+            } else if x <= -MIN {
+                stick = Some(west);
+            }
+        } else if y >= MIN {
+            stick = Some(north);
+        } else if y <= -MIN {
+            stick = Some(south);
         }
 
+        // Push if not a duplicate of previous push.
+        // Q: Is there a more terse / DRY way of doing this?
         match (&stick, self.vec.last()) {
             (Some(a), Some(b)) if a != b => self.vec.push(*a),
             (Some(a), None) => self.vec.push(*a),
             _ => (),
         }
-
-        /*
-        match &stick {
-            Some(_a) => {
-                if self.vec.len() > 0 {
-                    println!("vec is {:?}", self.vec);
-                }
-            },
-            _ => (),
-        }
-        */
     }
 
     fn check_zero(&mut self, gamepad: Gamepad) {
-        let min = 0.5;
-
-        if gamepad.value(Axis::RightStickX) < min && gamepad.value(Axis::RightStickX) > -min
-            && gamepad.value(Axis::RightStickY) < min && gamepad.value(Axis::RightStickY) > -min
-            && gamepad.value(Axis::LeftStickX) < min && gamepad.value(Axis::LeftStickX) > -min
-            && gamepad.value(Axis::LeftStickY) < min && gamepad.value(Axis::LeftStickY) > -min {
-                if self.vec.len() > 0 {
-                    match self.map.get(&self.vec) {
-                        Some(a) => println!("{:?}", *a),
-                        _ => (),
-                    }
-
-                    self.vec.clear();
-                }
+        if self.vec.len() > 1 // This allows combos across zero space (f.ex: Letter::Space).
+            && gamepad.value(Axis::RightStickX) < MAX
+            && gamepad.value(Axis::RightStickX) > -MAX
+            && gamepad.value(Axis::RightStickY) < MAX
+            && gamepad.value(Axis::RightStickY) > -MAX
+            && gamepad.value(Axis::LeftStickX) < MAX
+            && gamepad.value(Axis::LeftStickX) > -MAX
+            && gamepad.value(Axis::LeftStickY) < MAX
+            && gamepad.value(Axis::LeftStickY) > -MAX
+        {
+            if let Some(a) = self.map.get(&self.vec) {
+                println!("{:?}", *a)
+            }
+            self.vec.clear();
         }
     }
 
     fn process(&mut self, gamepad: Gamepad) {
+        // South button prints gamepad object.
         if gamepad.is_pressed(Button::South) {
             println!("GAMEPAD: {:#?}", gamepad);
         }
 
-        self.check_bounds_and_push(gamepad, Axis::LeftStickX, Axis::LeftStickY,
-                                   Stick::Left(Direction::North), 
-                                   Stick::Left(Direction::East), 
-                                   Stick::Left(Direction::South),
-                                   Stick::Left(Direction::West));
-        self.check_bounds_and_push(gamepad, Axis::RightStickX, Axis::RightStickY,
-                                   Stick::Right(Direction::North), 
-                                   Stick::Right(Direction::East), 
-                                   Stick::Right(Direction::South),
-                                   Stick::Right(Direction::West));
+        // Track left stick direction.
+        self.check_bounds_and_push(
+            gamepad.value(Axis::LeftStickX),
+            gamepad.value(Axis::LeftStickY),
+            Stick::Left(Direction::North),
+            Stick::Left(Direction::East),
+            Stick::Left(Direction::South),
+            Stick::Left(Direction::West),
+        );
 
+        // Track right stick direction.
+        self.check_bounds_and_push(
+            gamepad.value(Axis::RightStickX),
+            gamepad.value(Axis::RightStickY),
+            Stick::Right(Direction::North),
+            Stick::Right(Direction::East),
+            Stick::Right(Direction::South),
+            Stick::Right(Direction::West),
+        );
+
+        // Print any combo match and clear vec.
         self.check_zero(gamepad);
     }
 
     fn setup(&mut self) {
         self.map.insert(
             vec![Stick::Left(Direction::North), Stick::Left(Direction::West)],
-            Letter::A
+            Letter::A,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::North), Stick::Left(Direction::West), Stick::Left(Direction::South)],
-            Letter::B
+            vec![
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+            ],
+            Letter::B,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::North), Stick::Left(Direction::West), Stick::Left(Direction::South), Stick::Left(Direction::East)],
-            Letter::C
+            vec![
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+            ],
+            Letter::C,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::North), Stick::Left(Direction::West), Stick::Left(Direction::South), Stick::Left(Direction::East), Stick::Left(Direction::North)],
-            Letter::D
+            vec![
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+            ],
+            Letter::D,
         );
 
         self.map.insert(
             vec![Stick::Left(Direction::West), Stick::Left(Direction::South)],
-            Letter::E
+            Letter::E,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::West), Stick::Left(Direction::South), Stick::Left(Direction::East)],
-            Letter::F
+            vec![
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+            ],
+            Letter::F,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::West), Stick::Left(Direction::South), Stick::Left(Direction::East), Stick::Left(Direction::North)],
-            Letter::G
+            vec![
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+            ],
+            Letter::G,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::West), Stick::Left(Direction::South), Stick::Left(Direction::East), Stick::Left(Direction::North), Stick::Left(Direction::West)],
-            Letter::H
+            vec![
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+            ],
+            Letter::H,
         );
 
         self.map.insert(
             vec![Stick::Left(Direction::South), Stick::Left(Direction::East)],
-            Letter::I
+            Letter::I,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::South), Stick::Left(Direction::East), Stick::Left(Direction::North)],
-            Letter::J
+            vec![
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+            ],
+            Letter::J,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::South), Stick::Left(Direction::East), Stick::Left(Direction::North), Stick::Left(Direction::West)],
-            Letter::K
+            vec![
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+            ],
+            Letter::K,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::South), Stick::Left(Direction::East), Stick::Left(Direction::North), Stick::Left(Direction::West), Stick::Left(Direction::South)],
-            Letter::L
+            vec![
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+            ],
+            Letter::L,
         );
 
         self.map.insert(
             vec![Stick::Left(Direction::East), Stick::Left(Direction::North)],
-            Letter::M
+            Letter::M,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::East), Stick::Left(Direction::North), Stick::Left(Direction::West)],
-            Letter::N
+            vec![
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+            ],
+            Letter::N,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::East), Stick::Left(Direction::North), Stick::Left(Direction::West), Stick::Left(Direction::South)],
-            Letter::O
+            vec![
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+            ],
+            Letter::O,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::East), Stick::Left(Direction::North), Stick::Left(Direction::West), Stick::Left(Direction::South), Stick::Left(Direction::East)],
-            Letter::P
+            vec![
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::East),
+            ],
+            Letter::P,
         );
 
         self.map.insert(
             vec![Stick::Left(Direction::North), Stick::Left(Direction::East)],
-            Letter::Q
+            Letter::Q,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::North), Stick::Left(Direction::East), Stick::Left(Direction::South)],
-            Letter::R
+            vec![
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::South),
+            ],
+            Letter::R,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::North), Stick::Left(Direction::East), Stick::Left(Direction::South), Stick::Left(Direction::West)],
-            Letter::S
+            vec![
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::West),
+            ],
+            Letter::S,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::North), Stick::Left(Direction::East), Stick::Left(Direction::South), Stick::Left(Direction::West), Stick::Left(Direction::North)],
-            Letter::T
+            vec![
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::North),
+            ],
+            Letter::T,
         );
 
         self.map.insert(
             vec![Stick::Left(Direction::East), Stick::Left(Direction::South)],
-            Letter::U
+            Letter::U,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::East), Stick::Left(Direction::South), Stick::Left(Direction::West)],
-            Letter::V
+            vec![
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::West),
+            ],
+            Letter::V,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::East), Stick::Left(Direction::South), Stick::Left(Direction::West), Stick::Left(Direction::North)],
-            Letter::W
+            vec![
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::North),
+            ],
+            Letter::W,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::East), Stick::Left(Direction::South), Stick::Left(Direction::West), Stick::Left(Direction::North), Stick::Left(Direction::East)],
-            Letter::X
+            vec![
+                Stick::Left(Direction::East),
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::North),
+                Stick::Left(Direction::East),
+            ],
+            Letter::X,
         );
 
         self.map.insert(
             vec![Stick::Left(Direction::South), Stick::Left(Direction::West)],
-            Letter::Y
+            Letter::Y,
         );
         self.map.insert(
-            vec![Stick::Left(Direction::South), Stick::Left(Direction::West), Stick::Left(Direction::North)],
-            Letter::Z
+            vec![
+                Stick::Left(Direction::South),
+                Stick::Left(Direction::West),
+                Stick::Left(Direction::North),
+            ],
+            Letter::Z,
+        );
+
+        self.map.insert(
+            vec![Stick::Left(Direction::West), Stick::Left(Direction::East)],
+            Letter::Space,
         );
     }
 }
 
+#[allow(dead_code)]
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
@@ -261,9 +367,10 @@ fn main() {
     combo.setup();
 
     loop {
-        while let Some(Event { id, event: _, time: _ }) = gilrs.next_event() {
+        while let Some(Event { id, .. }) = gilrs.next_event() {
             active_gamepad = Some(id);
 
+            // Q: Can we get gamepad without using map?
             if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
                 combo.process(gamepad);
             }
